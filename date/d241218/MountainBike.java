93e24c457b6b01954444ff09c76a63ae7f8f461e
Kent Larsson <kent.larsson@gmail.com>
2024-12-18 08:02:29 +0100

    "Programmering 3" Live

---
+++ date/d241218/MountainBike.java
@@ -0,0 +1,11 @@
+package lektion1;
+
+public class MountainBike extends Bicycle {
+    public static void main(String... args) {
+        MountainBike cycle = new MountainBike();
+        cycle.speedUp(5);
+        cycle.printStates();
+        MountainBike cycle2 = new MountainBike();
+        cycle2.printStates();
+    }
+}
