852c5c79fb4dc54dac927527870d6dab043b8bb8
Kent Larsson <kent@c8.se>
2025-01-03 00:22:26 +0100

    241218 (#1)
    * autocommit complete 1242 (20)
    
    * autocommit complete 1259 (06)
    
    * autocommit complete 1306 (24)
    
    * autocommit complete 1311 (12)
    
    * autocommit complete 1334 (29)
    
    * autocommit complete 1335 (31)
    
    * autocommit complete 1336 (18)
    
    * autocommit complete 1338 (15)
    
    * autocommit complete 1340 (28)
    
    * autocommit complete 1341 (30)
    
    * autocommit complete 1411 (14)
    
    * autocommit complete 1412 (06)
    
    * autocommit complete 1413 (38)
    
    * autocommit complete 1415 (56)
    
    * autocommit complete 1418 (04)
    
    * autocommit complete 1420 (46)
    
    * remove
    
    * autocommit complete 1424 (11)
    
    * autocommit complete 1425 (13)
    
    * autocommit complete 1432 (17)
    
    * autocommit complete 1434 (44)
    
    * autocommit complete 1436 (12)
    
    * autocommit complete 1436 (43)
    
    * autocommit complete 1441 (36)
    
    * autocommit complete 1442 (48)
    
    * autocommit complete 1446 (31)
    
    * autocommit complete 1447 (12)
    
    * autocommit complete 1448 (09)
    
    * autocommit complete 1448 (21)
    
    * autocommit complete 1449 (18)
    
    * autocommit complete 1451 (45)
    
    * autocommit complete 1452 (37)
    
    * autocommit complete 1453 (44)
    
    * autocommit complete 1453 (51)
    
    * autocommit complete 1455 (33)
    
    * autocommit complete 1522 (20)
    
    * autocommit complete 1524 (52)
    
    * autocommit complete 1526 (34)

---
+++ MountainBike.java
@@ -0,0 +1,9 @@
+
+public class MountainBike extends Bicycle {
+    public static void main(String... args) {
+        MountainBike cycle = new MountainBike();
+        cycle.speedUp(5);
+        cycle.printStates();
+        MountainBike cycle2 = new MountainBike();
+    }
+}
