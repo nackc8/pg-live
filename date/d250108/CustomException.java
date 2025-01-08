0e32866b285a2ea577fba493413242ea8942dccd
Kent Larsson <kent@c8.se>
2025-01-08 13:49:14 +0100

    autocommit complete 1349 (14)

--- date/d250108/CustomException.java
+++ date/d250108/CustomException.java
@@ -9,15 +9,13 @@ class CustomException extends Exception {
         int outside = 10;
         System.out.println("försts, outside: " + outside);
         try {
-            int inside = 123;
-
+            outside = 123;
             System.out.println("innan exception, outside: " + outside);
             throw new CustomException("Ett specialfel inträffade!");
             // Körs aldrig pga exception:
             // System.out.println("efter throw exception, outside: " + outside);
         } catch (CustomException e) {
             System.out.println("i catch, outside: " + outside);
-            System.out.println("i catch, inside: " + inside);
             System.out.println(e.getMessage());
         }
         System.out.println("sist, outside: " + outside);
