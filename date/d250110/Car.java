48a62457c37eb35d2b4b8558826a5c8f2e439279
Kent Larsson <kent@c8.se>
2025-01-10 15:01:07 +0100

    autocommit complete 1501 (07)

--- date/d250110/Car.java
+++ date/d250110/Car.java
@@ -1,5 +1,7 @@
 package d250110;
 
+// Vid arv så utökar barnklassen föräldraklassen, "extends".
+
 public class Car extends AbstractVehicle {
     public Car(double angleRadians, int speed) {
         super(angleRadians, speed);
