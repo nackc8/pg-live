c0c10c26814d5ed401cf18fafc947d7cdfc90893
Kent Larsson <kent@c8.se>
2025-01-10 10:25:17 +0100

    autocommit complete 1025 (17)

--- date/d250110/Vehicle.java
+++ date/d250110/Vehicle.java
@@ -4,4 +4,8 @@ public class Vehicle {
     int angleRadians = 0;
     int speed = 0;
 
+    public Vehicle(int angleRadians, int speed) {
+        this.angleRadians = angleRadians;
+        this.speed = speed;
+    }
 }
