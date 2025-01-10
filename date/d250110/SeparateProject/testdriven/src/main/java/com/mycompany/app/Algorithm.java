81fb25dba876a5ae64f97e062061714367fe3adb
Kent Larsson <kent@c8.se>
2025-01-10 14:05:07 +0100

    autocommit complete 1405 (07)

--- date/d250110/SeparateProject/testdriven/src/main/java/com/mycompany/app/Algorithm.java
+++ date/d250110/SeparateProject/testdriven/src/main/java/com/mycompany/app/Algorithm.java
@@ -2,6 +2,11 @@ package com.mycompany.app;
 public class Algorithm {
     // low to high
     public int[] sort(int[] unsorted) {
+        int temp;
+        temp = unsorted[0];
+        unsorted[0] = unsorted[1];
+        unsorted[1] = temp;
+
         return unsorted;
     }
 }
