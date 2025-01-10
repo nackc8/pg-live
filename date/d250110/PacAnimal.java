7d5da555dbff930ff51013d2f545f98f2bc850da
Kent Larsson <kent@c8.se>
2025-01-10 14:55:56 +0100

    autocommit complete 1455 (56)

--- date/d250110/PacAnimal.java
+++ date/d250110/PacAnimal.java
@@ -1,5 +1,11 @@
 package d250110;
 
+// Ett interface används ofta för beteenden. I standardbiblioteket
+// så är List ett exempel på ett interface för klasser som man kan
+// behandla som en lista.
+//
+// Sedan finns det många olika listor som ArrayList och liknande.
+
 public interface PacAnimal {
     public void moveLeft();
     public void moveRight();
