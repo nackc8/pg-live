00f6335286dc6346c258926f5ee615da0eee97bb
Kent Larsson <kent@c8.se>
2025-01-10 11:13:32 +0100

    autocommit complete 1113 (32)

--- date/d250110/PacMain.java
+++ date/d250110/PacMain.java
@@ -3,4 +3,10 @@ package d250110;
 public class PacMain {
     Ghost g = new Ghost();
     Pacman p = new Pacman();
+
+    PacAnimal[] levelObjects = {g,p};
+
+    for (PacAnimal pa : levelObjects) {
+        //
+    }
 }
