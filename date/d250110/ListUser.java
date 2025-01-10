c5d5be4f22ecf6ee939c7f8eca500cbffb849478
Kent Larsson <kent@c8.se>
2025-01-10 13:17:25 +0100

    autocommit complete 1317 (25)

--- date/d250110/ListUser.java
+++ date/d250110/ListUser.java
@@ -6,7 +6,8 @@ import java.util.ArrayList;
 public class ListUser {
     public static void main(String[] args) {
         List list = new ArrayList();
-        list.add(1);
+        list.add("hello");
+        var uncasted = list.get(0);
         String s = (String) list.get(0);
     }
 }
