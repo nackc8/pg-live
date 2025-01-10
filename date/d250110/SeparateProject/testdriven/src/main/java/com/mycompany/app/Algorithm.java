a2f609d5ce8c63e1db553effa490aeb1864f2d51
Kent Larsson <kent@c8.se>
2025-01-10 13:47:28 +0100

    autocommit complete 1347 (28)

--- date/d250110/SeparateProject/testdriven/src/main/java/com/mycompany/app/Algorithm.java
+++ date/d250110/SeparateProject/testdriven/src/main/java/com/mycompany/app/Algorithm.java
@@ -3,7 +3,7 @@ package com.mycompany.app;
 /**
  * Hello world!
  */
-public class App {
+public class Algorithm {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
