f8439bc97378b32b2a6fb6199044481db3b793db
Kent Larsson <kent@c8.se>
2025-01-10 10:31:04 +0100

    autocommit complete 1031 (04)

--- date/d250110/Car.java
+++ date/d250110/Car.java
@@ -4,4 +4,9 @@ public class Car extends Vehicle {
     public Car(int angleRadians, int speed) {
         super(angleRadians, speed);
     }
+
+    @Override
+    public void signal() {
+        System.out.println("Honk!");
+    }
 }
