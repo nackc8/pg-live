42c5f79ea6090bfc161cd4e745b1f3a094797bd0
Kent Larsson <kent@c8.se>
2025-01-10 10:26:29 +0100

    autocommit complete 1026 (29)

--- date/d250110/Vehicle.java
+++ date/d250110/Vehicle.java
@@ -3,7 +3,7 @@ package d250110;
 public class Vehicle {
     int angleRadians = 0;
     int speed = 0;
-
+    
     public Vehicle(int angleRadians, int speed) {
         this.angleRadians = angleRadians;
         this.speed = speed;
