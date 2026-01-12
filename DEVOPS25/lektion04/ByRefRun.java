6afd591df138cf85e3a6021f7e7c5463f26d0fca
Kent Larsson <kent@c8.se>
2026-01-12 13:27:13 +0100

    autocommit complete 1327 (13)

--- DEVOPS25/lektion04/ByRefRun.java
+++ DEVOPS25/lektion04/ByRefRun.java
@@ -1,8 +1,12 @@
 
 public class ByRefRun {
-    
+    public static void increase(ByRef b) {
+        b.age += 1;
+    }
+
     public static void main(String[] args) {
         var obj = new ByRef();
+        increase(obj);
         System.out.println(obj.age);
     }
 }
