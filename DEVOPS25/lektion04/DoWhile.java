dbe28858f221c75860379802e3ba684cbe612c91
Kent Larsson <kent@c8.se>
2026-01-12 11:17:35 +0100

    autocommit complete 1117 (35)

---
+++ DEVOPS25/lektion04/DoWhile.java
@@ -0,0 +1,13 @@
+
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int[] numbers = {8,63,82,98,9,1,-100,200,150};
+
+        do {
+
+        } while (numbers)
+
+        System.out.println(x);
+    }
+}
