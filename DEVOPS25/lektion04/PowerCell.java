05ffcd6b725cc072ebd9345ef108b6addaee56ed
Kent Larsson <kent@c8.se>
2026-01-12 09:44:44 +0100

    autocommit complete 0944 (44)

--- DEVOPS25/lektion04/PowerCell.java
+++ DEVOPS25/lektion04/PowerCell.java
@@ -5,6 +5,8 @@ public class PowerCell {
 
     float power = 0.0f;
 
+    public 
+
     public static void main(String[] args) {
         PowerCell cell1 = new PowerCell();
         cell1.power = 0.3f;
