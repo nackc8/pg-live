4ffd088216dea3fd9c1a0217134b692baa2f9111
Kent Larsson <kent@c8.se>
2026-01-12 09:54:17 +0100

    autocommit complete 0954 (17)

--- DEVOPS25/lektion04/PowerCellMain.java
+++ DEVOPS25/lektion04/PowerCellMain.java
@@ -2,7 +2,7 @@
 
 public class PowerCellMain {
     public static void main(String[] args) {
-        PowerCell cell1 = new PowerCell();
+        PowerCell cell1 = new PowerCell(0.7f);
         System.out.println(cell1.getPower());
     }
 }
