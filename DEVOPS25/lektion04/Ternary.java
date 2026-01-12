7ed74ddbeceba45b4572c4c64995d3713f9b406c
Kent Larsson <kent@c8.se>
2026-01-12 11:09:02 +0100

    autocommit complete 1109 (02)

--- DEVOPS25/lektion04/Ternary.java
+++ DEVOPS25/lektion04/Ternary.java
@@ -3,13 +3,18 @@
 public class Ternary {
     public static void main(String[] args) {
         boolean big = true;
-        var size = 100 + (big ? 50: 0);
+        boolean dangerous = true;
+
+        var size = 100 + (big ? 50: 0) + (dangerous ? 200 : 0);
 
         System.out.println(size);
 
         var sizeNonTernary = 100;
         if (big) {
             sizeNonTernary += 50;
+        }
+        if (dangerous) {
+            sizeNonTernary += 200;
         }
 
         System.out.println(sizeNonTernary);
