bbda0c5483301cbecc894f941afde78b9e532d50
Kent Larsson <kent@c8.se>
2026-01-12 11:22:36 +0100

    autocommit complete 1122 (36)

--- DEVOPS25/lektion04/DoWhile.java
+++ DEVOPS25/lektion04/DoWhile.java
@@ -4,6 +4,7 @@ public class DoWhile {
     public static void main(String[] args) {
         int number = 100_000;
 
+        // MINST en gång, till skillnad från while .. do
         do {
             number += 200;
             System.out.println("number: " + number);
