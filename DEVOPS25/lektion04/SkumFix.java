1d5be68974afe79b2f38401fcdf60abab70b38a3
Kent Larsson <kent@c8.se>
2026-01-12 10:53:36 +0100

    autocommit complete 1053 (36)

--- DEVOPS25/lektion04/SkumFix.java
+++ DEVOPS25/lektion04/SkumFix.java
@@ -4,11 +4,11 @@ public class SkumFix {
     public static void main(String[] args) {
         int x = 10;
         // Post = ändra direkt EFTERÅT
-        System.out.printf("x = %d\n", x++);
+        System.out.printf("x = %d\n", ++x);
         System.out.printf("x = %d\n", x);
-        System.out.printf("x = %d\n", x--);
-        // Brain overload
-        x = 10;
-        System.out.printf("x = %d\n", x++ + x++);
+        System.out.printf("x = %d\n", --x);
+        // // Brain overload
+        // x = 10;
+        // System.out.printf("x = %d\n", x++ + x++);
     }
 }
