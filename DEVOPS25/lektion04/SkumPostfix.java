e2d5d3b7862e0ec8e534d89338dc933dabb6ab1f
Kent Larsson <kent@c8.se>
2026-01-12 10:46:06 +0100

    autocommit complete 1046 (06)

--- DEVOPS25/lektion04/SkumPostfix.java
+++ DEVOPS25/lektion04/SkumPostfix.java
@@ -4,5 +4,6 @@ public class SkumPostfix {
     public static void main(String[] args) {
         int x = 10;
         System.out.printf("x = %d\n", x++);
+        System.out.printf("x = %d\n", x);
     }
 }
