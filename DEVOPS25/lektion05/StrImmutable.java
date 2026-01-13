5668c935c3247cb8afa6c9595c4c5025bb153fa2
Kent Larsson <kent@c8.se>
2026-01-13 09:40:16 +0100

    autocommit complete 0940 (16)

--- DEVOPS25/lektion05/StrImmutable.java
+++ DEVOPS25/lektion05/StrImmutable.java
@@ -2,6 +2,10 @@
 
 public class StrImmutable {
     public static void main(String[] args) {
+        String namn = "Xi";
+
+        namn = "Pelle";
+
         int x = 1;
         System.out.println(x);
     }
