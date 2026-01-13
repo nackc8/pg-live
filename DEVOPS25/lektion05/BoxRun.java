e716bc034f1fd0db2ed45245f5ccc2964eea4848
Kent Larsson <kent@c8.se>
2026-01-13 10:38:01 +0100

    autocommit complete 1038 (01)

--- DEVOPS25/lektion05/BoxRun.java
+++ DEVOPS25/lektion05/BoxRun.java
@@ -1,11 +1,15 @@
 public class BoxRun {
     public static void main(String[] args) {
         // Box<String> lada = new Box<String>();
-        // var greeting = "Hej";
+        var greeting = "Hej";
         // lada.set(greeting);
 
         // var iboxen = lada.get();
 
-        // Exempel med Box som i
+        // Exempel med Box som inte är generisk och lagrar typen Object
+
+        Box obox = new Box();
+        obox.set(greeting);
+        var iobjladan = obox.get();
     }
 }
